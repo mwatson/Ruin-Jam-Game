069eaa930cd1c048d7d449e61331bc68b96072f9
Mike <mike.watson@gmail.com>
2014-09-03 17:03:51 -0700

    Procedural hair, mostly

--- app/game/entity/player.js
+++ app/game/entity/player.js
@@ -14,15 +14,49 @@
                 this.reset = function() {
                 };
 
-                this.init = function() {
+                function setPixel(imgData, x, y, r, g, b, a) {
+                        var index = (x + y * imgData.width) * 4;
+                        imgData.data[index +  0] = r;
+                        imgData.data[index +  1] = g;
+                        imgData.data[index +  2] = b;
+                        imgData.data[index +  3] = a;
+                }
 
-                        function setPixel(imgData, x, y, r, g, b, a) {
-                                var index = (x + y * imgData.width) * 4;
-                                imgData.data[index +  0] = r;
-                                imgData.data[index +  1] = g;
-                                imgData.data[index +  2] = b;
-                                imgData.data[index +  3] = a;
+                function generateHair(imgData, hairDone, hairGrid, color, x, y) {
+
+                        if(!_.isUndefined(hairDone[x + '_' + y])) {
+                                return;
                         }
+
+                        if(_.isUndefined(hairGrid[y]) || _.isUndefined(hairGrid[y][x])) {
+                                return;
+                        }
+
+                        hairDone[x + '_' + y] = true;
+
+                        if(hairGrid[y][x] == 0) {
+                                return;
+                        }
+
+                        var ch = App.Tools.rand(1, 5);
+                        if(ch > hairGrid[y][x]) {
+                                return;
+                        }
+
+                        setPixel(imgData, x, y, color.r, color.g, color.b, 255);
+
+                        for(var i = -1; i <= 1; i++) {
+                                for(var j = -1; j <= 1; j++) {
+                                        if(j == 0 && i == 0) {
+                                                continue;
+                                        }
+
+                                        generateHair(imgData, hairDone, hairGrid, color, x + j, y + i);
+                                }
+                        }
+                }
+
+                this.init = function() {
 
                         var props = App.Definitions.get('Entity', 'player');
 
@@ -49,15 +83,24 @@
                             shirtshadow = { r: 175, g: 119, b: 0 }, 
                             pants = { r: 0, g: 6, b: 81 }, 
                             eyes = { r: 0, g: 0, b: 0 }, 
-                            grid = App.Defs.PlayerSprites[gId].adult;
+                            bodyGrid = App.Defs.PlayerSprites[gId].adult.bodyMap, 
+                            hairGrid = App.Defs.PlayerSprites[gId].adult.hairMap, 
+                            x, y, its, 
+                            hairStart = { x: -1, y: -1 }, 
+                            hairDone = {};
 
-                        for(var y = 0; y < grid.length; y++) {
-                                for(var x = 0; x < grid[y].length; x++) {
+                        for(y = 0; y < bodyGrid.length; y++) {
+                                for(x = 0; x < bodyGrid[y].length; x++) {
                                         var cols;
-                                        if(!grid[y][x]) {
+                                        if(hairGrid[y][x] == 5 && hairStart.x == -1) {
+                                                hairStart.x = x;
+                                                hairStart.y = y;
+                                        }
+
+                                        if(!bodyGrid[y][x]) {
                                                 continue;
                                         }
-                                        switch(grid[y][x]) {
+                                        switch(bodyGrid[y][x]) {
                                                 case 1:
                                                         cols = skin;
                                                         break;
@@ -77,6 +120,12 @@
                                         setPixel(imgData, x, y, cols.r, cols.g, cols.b, 255);
                                 }
                         }
+
+                        its = 0;
+                        x = hairStart.x;
+                        y = hairStart.y;
+
+                        generateHair(imgData, hairDone, hairGrid, hair, x, y);
 
                         ctx.putImageData(imgData, 0, 0);
 
